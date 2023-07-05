3bfa9ce741b90e78f140eb93e4a09ddbfd8a8f91
jvandene <jvandene@pcatmag16.cern.ch>
2023-07-05 20:19:59 +0200

    makeplots for quench propagation

---
+++ plotBET.m
@@ -0,0 +1,88 @@
+figure(2)
+
+    set(gcf,'Color','w')
+   s1 = 't = ';
+   s3 = ' s';
+
+
+% v = VideoWriter('Vground5.mp4','MPEG-4');
+% v.FrameRate = 1;
+% open(v);
+% 
+% for k = 1:size(VGroundVectorhistory,2)
+%     if tArrayhistory(k) > 40
+%         break
+%     end
+% 
+% 
+%    titlestring = strcat(s1,num2str(tArrayhistory(k),3),s3);
+% 
+%    plot(1e6.*(VGroundVectorhistory(:,k))./(5.*2.*pi.*0.12));
+% 
+%    title(titlestring)
+% 
+%    legend('E [\muV/m]',Location='southwest')
+%    grid on
+%    %    ylim([-4000 8000])
+%    % xlim([1 450])
+% ylabel('E [\muV/m]')
+%    xlabel('line element nr [-]')
+%    frame = getframe(gcf);
+%    writeVideo(v,frame);
+% 
+% end
+% 
+% close(v);
+
+
+
+
+
+
+
+for k = 1:size(VGroundVectorhistory,2)-1
+round(length(VGroundVector)*1/10)
+round(length(VGroundVector)*9/10)
+Ecoil(k) = 1e6*(VGroundVectorhistory(round(length(VGroundVector)*1/10),k)-VGroundVectorhistory(round(length(VGroundVector)*9/10),k))./(4*2*pi*0.12);
+end
+figure(4)
+plot(tArrayhistory(1:length(Ecoil)),Ecoil)
+xlabel('time [s]')
+ylabel('E [\muV/m]')
+
+figure(5)
+plot(tArrayhistory(1:length(centerBhistory)),centerBhistory)
+xlabel('time [s]')
+ylabel('B [T]')
+
+figure(6)
+plot(tArrayhistory(1:length(temperatureRingBottomhistory)),temperatureRingBottomhistory)
+xlabel('time [s]')
+ylabel('T [K]')
+
+
+% N = 31    ;
+% 
+% for i = 1:N
+%     figure(1)  
+%   %  imshow(processo(:,:,1,i))
+% 
+% plot(IArrayhistory(:,N))
+% 
+%       F(i) = getframe(gcf) ;
+%       drawnow
+%     end
+%   % create the video writer with 1 fps
+%   writerObj = VideoWriter('myVideo2.avi');
+%   writerObj.FrameRate = 1;
+%   % set the seconds per image
+% % open the video writer
+% open(writerObj);
+% % write the frames to the video
+% for i=1:length(F)
+%     % convert the image to a frame
+%     frame = F(i) ;    
+%     writeVideo(writerObj, frame);
+% end
+% % close the writer object
+% close(writerObj);
